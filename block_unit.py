1ca673d916ced7d086486aa35e61ed88efdde2cc
Abdelrahman Mohamed Salah <67156865+AbdelrahmanBedour@users.noreply.github.com>
2022-02-10 00:15:06 +0200

    Update block_unit.py

--- block_unit.py
+++ block_unit.py
@@ -1,35 +1,58 @@
-import datetime
-import hashlib
-import numpy as np
+from block_unit import Block
 
 
-class Block:
-    blockNo = 0
-    data = None
-    next = None
-    nonce = np.uint64(0)
-    previous_hash = 0x0
-    timestamp = datetime.datetime.now()
+class Blockchain:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, x):
+        self.diff=x*4
+        self.maxNonce = 2 ** 64
+        self.block = Block("Genesis")
+        dummy = self.head = self.block
+        self.target = 2 ** (256 - self.diff)
+        for n in range(self.maxNonce):
+            if int(self.block.hash(), 16) <= self.target:
+                break
+            else:
+                self.block.nonce =self.block.nonce+ 1
 
-    def add_transaction(self,trans):
-        if len(self.data)==0:
-            self.data = self.data + trans
+
+    def Set_N (self,N):
+        self.diff = N*4
+        self.target = 2 ** (256 - self.diff)
+
+    def is_valid_proof(self, block):
+        if int(block.hash(), 16) <= self.target:
+            return True
         else:
-            self.data = self.data + '\n'+trans
+            return False
 
-    def hash(self):
-        h = hashlib.sha256()
-        h.update(
-        str(self.nonce).encode('utf-8') +
-        str(self.data).encode('utf-8') +
-        str(self.previous_hash).encode('utf-8') +
-        str(self.timestamp).encode('utf-8') +
-        str(self.blockNo).encode('utf-8')
-        )
-        return h.hexdigest()
+    def add(self, block):
+        if block.previous_hash == self.block.hash() and self.is_valid_proof( block) :
+            self.block.next = block
+            self.block = self.block.next
+    def chain_len(self):
+        counter = 0
+        temp_head = self.head
 
-    def __str__(self):
-        return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNo) + "\nBlock Data: " + str(self.data) + "\nNonce: " + str(int(self.nonce)) + "\n--------------"
+        while (temp_head != None):
+            counter += 1
+            temp_head = temp_head.next
+        return counter
+
+    def print_chain(self):
+        Temp_head = self.head
+        while Temp_head != None:
+            # print(blockchain.head.previous_hash)
+            print(Temp_head)
+            Temp_head = Temp_head.next
+
+    def mine(self, block):
+        block.previous_hash = self.block.hash()
+        block.blockNo = self.block.blockNo + 1
+        for n in range(self.maxNonce):
+            if int(block.hash(), 16) <= self.target:
+                self.add(block)
+                # print(block)
+                break
+            else:
+                block.nonce =block.nonce+ 1
